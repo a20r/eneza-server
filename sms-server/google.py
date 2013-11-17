5e7dba8625166761dc2ed143530ac28b30b59866
miskinh <henry.miskin@talktalk.net>
2013-11-17 01:42:22 +0000

    Logic to decompose JSON (problems present)

--- sms-server/google.py
+++ sms-server/google.py
@@ -1,17 +1,76 @@
 import urllib,json
 
-url = "http://www.google.com/dictionary/json?callback=a&sl=en&tl=en&q=armchair"
-openURL = urllib.urlopen(url)
-defineString = openURL.read()
+#global variables
+DEBUG = True
+URL = "http://www.google.com/dictionary/json?callback=json&sl=en&tl=en&"
 
-#remove all characters outside parenthesese
-firstIndex = defineString.find("{")
-lastIndex = defineString.rfind("}")+1
-jsonString = defineString[firstIndex:lastIndex]
+class Google():
 
-#escape all backslashes
-jsonString = jsonString.replace("\\","\\\\")
+	def __init__(self,searchQuery):
 
-#return json object from string
-content = json.loads(jsonString)
+		self.searchQuery = searchQuery
 
+		self.setURL()
+		self.setContent()
+		self.setResponse()
+
+	def setURL(self):
+		"method to set the url for request"
+
+		searchQuery = self.searchQuery
+
+		query = urllib.urlencode({"q":searchQuery})
+		url = URL+query
+
+		if DEBUG:
+			print(url)
+
+		self.url = url
+
+	def setContent(self):
+		"method to set the content of the class"
+
+		url = self.url
+
+		openURL = urllib.urlopen(url)
+		defineString = openURL.read()
+
+		#remove all characters outside parenthesese
+		firstIndex = defineString.find("{")
+		lastIndex = defineString.rfind("}")+1
+		jsonString = defineString[firstIndex:lastIndex]
+
+		#escape all backslashes
+		jsonString = jsonString.replace("\\","\\\\")
+
+		#return json object from string
+		content = json.loads(jsonString)
+
+		if DEBUG:
+			print content
+
+		self.content = content
+
+	def setResponse(self):
+		"method to set the responce to send"
+
+		content = self.content
+
+		response = None
+
+		for i in range(10):
+			try:
+				response = content["primaries"][0]["entries"][i]["terms"][0]["text"]
+			except:
+				pass
+			i+=1
+
+		if DEBUG:
+			print response
+
+		self.response = response
+
+
+if __name__ == "__main__":
+	#Google("drum and bass")
+	Google("house")
